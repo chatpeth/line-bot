48f85434143c7a60dd68f217619834b68ee28cba
chatpeth <36184704+chatpeth@users.noreply.github.com>
2018-11-16 13:07:47 +0700

    rebuild

--- bot.php
+++ bot.php
@@ -12,8 +12,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = "Hello\r\n" . $event['message']['text'];
-			//$text = 'Chatpeth Kenanan'
+			//$text = "Hello" . $event['message']['text'];
+			$text = "Chatpeth Kenanan\r\n"
 			
 			$replyToken = $event['replyToken'];
 
